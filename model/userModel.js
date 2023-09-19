29333ae781f14e4146f168fe543e9160266010fa
LiorAharon98 <lior.ah98@gmail.com>
2023-09-19 14:51:12 +0300

    inital

---
+++ model/userModel.js
@@ -0,0 +1,34 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema({
+  visited: Date,
+  available: Date,
+  contact : String,
+  rentOrSell: String,
+  type: String,
+  condition: String,
+  city: String,
+  neighborhood: String,
+  street: String,
+  squareMeter: Number,
+  rooms: Number,
+  floor: Number,
+  maxFloor: Number,
+  balcony: Number,
+  serviceBalcony: Number,
+  parking: Number,
+  warehouse: Number,
+  bathrooms: Number,
+  bathtub: String,
+  showerStall: String,
+  elevator: Number,
+  airConditioner: String,
+  furniture: String,
+  airDirections: String,
+  Extended: Array,
+  propertyTax: Number,
+  committee: Number,
+  price: Number,
+});
+
+const UserModel = mongoose.model("users", userSchema);
+module.exports = UserModel;
