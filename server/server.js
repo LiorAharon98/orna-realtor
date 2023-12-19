7fd33950f6102a3f34d0841abe988fd3b3412afb
LiorAharon98 <lior.ah98@gmail.com>
2023-12-19 14:23:12 +0200

    add delete property

--- server/server.js
+++ server/server.js
@@ -3,9 +3,11 @@ const mongoose = require("mongoose");
 const userRoute = require("./routes/index");
 const setServerConfiguration = require("./config");
 const app = express();
+const dbUrl =
+process.env.NODE_ENV == "production"
+? "mongodb+srv://liors-database:lior.ah980@cluster0.iybrzvm.mongodb.net/orna_realtor?retryWrites=true&w=majority"
+: "mongodb://localhost:27017/orna-realtor";
 setServerConfiguration(app);
-mongoose.connect(
-  "mongodb+srv://liors-database:lior.ah980@cluster0.iybrzvm.mongodb.net/orna_realtor?retryWrites=true&w=majority"
-);
+mongoose.connect(dbUrl);
 app.use("/", userRoute);
 app.listen(process.env.PORT || 4000);
