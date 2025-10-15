66779e80ffa889b4fdc9d56df1451446b5b924bf
LiorAharon98 <lior.ah98@gmail.com>
2025-10-15 10:38:44 +0300

    new version

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 const setServerConfiguration = (app) => {
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
   app.use(cors());
-  
 };
 module.exports = setServerConfiguration;
