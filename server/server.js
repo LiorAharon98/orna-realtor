e94369a856d1bdb6105cc433fcf51d671dd23858
LiorAharon98 <lior.ah98@gmail.com>
2025-10-15 10:51:16 +0300

    new version

--- server/server.js
+++ server/server.js
@@ -3,10 +3,8 @@ const mongoose = require("mongoose");
 const userRoute = require("./routes/index");
 const setServerConfiguration = require("./config");
 const app = express();
-const dbUrl =
-process.env.NODE_ENV == "production"
-? "mongodb+srv://liors-database:lior.ah980@cluster0.iybrzvm.mongodb.net/orna_realtor?retryWrites=true&w=majority"
-: "mongodb://localhost:27017/orna-realtor";
+
+const dbUrl = process.env.NODE_ENV == "production" ? process.env.DB_PRODUCTION : process.env.DB_LOCAL;
 setServerConfiguration(app);
 mongoose.connect(dbUrl);
 app.use("/", userRoute);
