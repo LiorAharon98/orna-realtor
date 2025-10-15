57bacc98d1d122c7b112279c9d8527d227bd78bd
LiorAharon98 <lior.ah98@gmail.com>
2025-10-15 10:15:07 +0300

    new version

--- client/src/routes/Routes1.jsx
+++ client/src/routes/Routes1.jsx
@@ -1,22 +1,25 @@
-import { BrowserRouter, Route,Routes,HashRouter } from "react-router-dom";
+import { Route, Routes, HashRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import Admin from "../pages/Admin";
 import AddProperty from "../pages/AddProperty";
 import SpecificProperty from "../pages/SpecificProperty";
-const Routes1 = ({children}) => {
-  const isUserLogged = JSON.parse(localStorage.getItem('orna'))
+import { useSelector } from "react-redux";
+const Routes1 = ({ children }) => {
+  const isUserLogged = JSON.parse(localStorage.getItem("orna"));
+  const {isLoggedIn} = useSelector((state) => state.user);
+ 
   return (
     <HashRouter>
-    {children}
+      {children}
       <Routes>
         <Route path="/" element={<Home />}></Route>
-        {isUserLogged &&
-        <>
-        <Route path="/add-property" element={<AddProperty />}></Route>
-        <Route path="/admin" element={<Admin />}></Route>
-        <Route path="/specific-property" element={<SpecificProperty />}></Route>
-        </>
-        }
+        {isUserLogged && (
+          <>
+            <Route path="/add-property" element={<AddProperty />}></Route>
+            <Route path="/admin" element={<Admin />}></Route>
+            <Route path="/specific-property" element={<SpecificProperty />}></Route>
+          </>
+        )}
       </Routes>
     </HashRouter>
   );
