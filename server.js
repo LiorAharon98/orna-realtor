c48d59a0b8ddbadc7dbdb9cc7333d82c35063cd9
LiorAharon98 <lior.ah98@gmail.com>
2025-10-15 10:48:55 +0300

    new version

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const userRoute = require("./routes/index");
 const setServerConfiguration = require("./config");
 const app = express();
+
 const dbUrl = process.env.NODE_ENV == "production" ? process.env.DB_PRODUCTION : process.env.DB_LOCAL;
 setServerConfiguration(app);
 mongoose.connect(dbUrl);
