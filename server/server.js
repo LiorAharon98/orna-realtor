64cd0ce9a91b87fedd7b4e5b5bf3263d24d3c076
LiorAharon98 <lior.ah98@gmail.com>
2026-05-31 18:16:27 +0300

    updating api to aws

--- server/server.js
+++ server/server.js
@@ -7,5 +7,8 @@ const app = express();
 const dbUrl = process.env.NODE_ENV == "production" ? process.env.DB_PRODUCTION : process.env.DB_LOCAL;
 setServerConfiguration(app);
 mongoose.connect(dbUrl);
+app.get("/", (req, res) => {
+  res.send("Api is working!");
+});
 app.use("/", userRoute);
 app.listen(process.env.PORT || 4000);
