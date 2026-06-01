4d28d6e98d82f863d7de60ea80e8f3c9567ff8f0
LiorAharon98 <lior.ah98@gmail.com>
2026-06-01 15:21:00 +0300

    updating api to aws

--- client/src/serverUrl.js
+++ client/src/serverUrl.js
@@ -1,6 +1,6 @@
 const serverUrl = (routeName) => {
  
-  return process.env.NODE_ENV !== "development"
+  return process.env.NODE_ENV === "development"
     ? `${process.env.REACT_APP_LOCAL_URL}/${routeName}`
     : `${process.env.REACT_APP_PRODUCTION_API}/${routeName}`;
 };
